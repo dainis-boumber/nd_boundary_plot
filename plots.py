2c069277cd8a2628fc791cb88f2d5750297d8355
Dainis Boumber <dainis.boumber@gmail.com>
2017-10-27 05:34:07 -0500

    converted all to runnable module

--- plots.py
+++ plots.py
@@ -1,5 +1,10 @@
 from sklearn.neighbors.classification import KNeighborsClassifier
 from sklearn.manifold.t_sne import TSNE
+import matplotlib.pyplot as plt
+from sklearn.datasets.base import load_digits
+from sklearn.datasets.base import load_iris
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.neural_network import MLPClassifier
 import numpy as np
 
 # draw a projection onto 2D using vornoi tessalation. Approximates highly dimensional boundaries in 2D
@@ -16,3 +21,21 @@ def nd_boundary_plot(X, y, y_predicted, ax, resolution = 256, **kwargs):
     ax.scatter(X_train[:,0], X_train[:,1], c=y)
     ax.set(**kwargs)
 
+def main():
+    X, y = load_digits(n_class=10, return_X_y=True)
+    model = MLPClassifier(hidden_layer_sizes=(100,100)).fit(X, y)
+    y_predicted = model.predict(X)
+    ax0_0 = plt.subplot2grid([2, 1], (0, 0))
+    nd_boundary_plot(X, y, y_predicted, ax0_0, resolution=100, adjustable='box-forced', aspect='equal')
+
+    iris = load_iris()
+    X, y = iris.data, iris.target
+    model = LogisticRegression().fit(X, y)
+    y_predicted = model.predict(X)
+    ax1_0 = plt.subplot2grid([2, 1], (1, 0))
+    nd_boundary_plot(X, y, y_predicted, ax1_0, resolution=100, adjustable='box-forced', aspect='equal')
+    #plt.tight_layout()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
