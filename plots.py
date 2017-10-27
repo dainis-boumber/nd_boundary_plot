cb2523d42114736e7ccbf98f6e9f23a6786b8c14
Dainis Boumber <dainis.boumber@gmail.com>
2017-10-27 06:43:28 -0500

    Fixed a bug where len(test) had to be == len(train)

--- plots.py
+++ plots.py
@@ -9,16 +9,16 @@ import numpy as np
 
 # draw a projection onto 2D using vornoi tessalation. Approximates highly dimensional boundaries in 2D
 # see https://pure.uva.nl/ws/files/2110683/164710_431596.pdf
-def nd_boundary_plot(X, y, y_predicted, ax, resolution = 256, **kwargs):
-    X_train = TSNE(n_components=2).fit_transform(X)
-    stdx, stdy = np.std(X_train[:,0]), np.std(X_train[:, 1])
-    xmin, xmax = np.min(X_train[:,0]) - stdx, np.max(X_train[:,0]) + stdx
-    ymin, ymax = np.min(X_train[:,1]) - stdy, np.max(X_train[:,1]) + stdy
+def nd_boundary_plot(X_tst, y_predicted, ax, resolution = 256, **kwargs):
+    X_tst = TSNE(n_components=2).fit_transform(X_tst)
+    stdx, stdy = np.std(X_tst[:,0]), np.std(X_tst[:, 1])
+    xmin, xmax = np.min(X_tst[:,0]) - stdx, np.max(X_tst[:,0]) + stdx
+    ymin, ymax = np.min(X_tst[:,1]) - stdy, np.max(X_tst[:,1]) + stdy
     xx, yy = np.meshgrid(np.linspace(xmin, xmax, resolution), np.linspace(ymin, ymax, resolution))
-    background_model = KNeighborsClassifier(n_neighbors=1).fit(X_train, y_predicted)
+    background_model = KNeighborsClassifier(n_neighbors=1).fit(X_tst, y_predicted)
     voronoi = background_model.predict(np.c_[xx.ravel(), yy.ravel()]).reshape((resolution, resolution))
     ax.contourf(xx, yy, voronoi)
-    ax.scatter(X_train[:,0], X_train[:,1], c=y)
+    ax.scatter(X_tst[:,0], X_tst[:,1], c=y_predicted)
     ax.set(**kwargs)
 
 def main():
@@ -26,14 +26,14 @@ def main():
     model = MLPClassifier(hidden_layer_sizes=(100,100)).fit(X, y)
     y_predicted = model.predict(X)
     ax0_0 = plt.subplot2grid([2, 1], (0, 0))
-    nd_boundary_plot(X, y, y_predicted, ax0_0, resolution=100, adjustable='box-forced', aspect='equal')
+    nd_boundary_plot(X, y_predicted, ax0_0, resolution=100, adjustable='box-forced', aspect='equal')
 
     iris = load_iris()
     X, y = iris.data, iris.target
     model = LogisticRegression().fit(X, y)
     y_predicted = model.predict(X)
     ax1_0 = plt.subplot2grid([2, 1], (1, 0))
-    nd_boundary_plot(X, y, y_predicted, ax1_0, resolution=100, adjustable='box-forced', aspect='equal')
+    nd_boundary_plot(X, y_predicted, ax1_0, resolution=100, adjustable='box-forced', aspect='equal')
     #plt.tight_layout()
     plt.show()
 
