12dd5fdcef68344bac04b08bac14ab0db8d8bf8b
Dainis Boumber <dainis.boumber@gmail.com>
2017-11-15 00:44:37 -0600

    submodule commit

--- plots.py
+++ plots.py
@@ -11,7 +11,7 @@ import numpy as np
 
 # draw a projection onto 2D using vornoi tessalation. Approximates highly dimensional boundaries in 2D
 # see https://pure.uva.nl/ws/files/2110683/164710_431596.pdf
-def nd_boundary_plot(X_tst, model, mesh_limits, ax, resolution = 256):
+def nd_boundary_plot(X_tst, y_predicted, model, ax, resolution = 256):
     if len(X_tst.shape) != 2:
         raise ValueError("X must be ndarray of the form [nsamples, nfeatures]")
     if X_tst.shape[0] < 2:
@@ -22,26 +22,27 @@ def nd_boundary_plot(X_tst, model, mesh_limits, ax, resolution = 256):
         raise ValueError("Classification must be at least binary")
     #done with sanity checks
 
-    if X_tst.shape[0] == 2: #2 dimensions
-        xmin, xmax, ymin, ymax = mesh_limits
-        xx, yy = np.meshgrid(np.arange(xmin, xmax, .05), np.arange(ymin, ymax, .05))
+    if X_tst.shape[1] == 2: #2 dimensions
+        X = X_tst
+        xmin, xmax = np.min(X[:,0]), np.max(X[:,0])
+        ymin, ymax = np.min(X[:,1]), np.max(X[:,1])
+        xx, yy = np.meshgrid(np.linspace(xmin, xmax, resolution), np.linspace(ymin, ymax, resolution))
+
         if hasattr(model, "decision_function") or len(model.classes_) != 2: #model does not comute posterior or hard to graph
             Z = model.predict(np.c_[xx.ravel(), yy.ravel()])
         else:
             Z = model.predict_proba(np.c_[xx.ravel(), yy.ravel()])[:, 1]
-
-        Z = Z.reshape(xx.shape)
-        ax.contourf(xx, yy, Z, alpha=.3)
     else:#lots of dimensions
-        y_predicted = model.predict(X_tst)
-        X_transormed= TSNE(n_components=2).fit_transform(X_tst)
-        xmin, xmax = np.min(X_transormed[:,0]) - .5, np.max(X_transormed[:,0]) + .5
-        ymin, ymax = np.min(X_transormed[:,1]) - .5, np.max(X_transormed[:,1]) + .5
+        X = TSNE(n_components=2).fit_transform(X_tst)
+        background_model = KNeighborsClassifier(n_neighbors=1).fit(X, y_predicted)
+        xmin, xmax = np.min(X[:,0]), np.max(X[:,0])
+        ymin, ymax = np.min(X[:,1]), np.max(X[:,1])
         xx, yy = np.meshgrid(np.linspace(xmin, xmax, resolution), np.linspace(ymin, ymax, resolution))
-        background_model = KNeighborsClassifier(n_neighbors=1).fit(X_transormed, y_predicted)
-        voronoi = background_model.predict(np.c_[xx.ravel(), yy.ravel()]).reshape((resolution, resolution))
-        ax.contourf(xx, yy, voronoi)
-        ax.scatter(X_transormed[:,0], X_transormed[:,1], c=y_predicted)
+        Z = background_model.predict(np.c_[xx.ravel(), yy.ravel()])
+
+    Z = Z.reshape((resolution, resolution))
+    ax.contourf(xx, yy, Z, alpha=.3)
+    ax.scatter(X[:,0], X[:,1], c=y_predicted)
 
 def main():
 
@@ -50,26 +51,13 @@ def main():
 
     model = MLPClassifier(hidden_layer_sizes=(100,100)).fit(X, y)
     ax = plt.subplot2grid([2, 2], (0, 0))
-    nd_boundary_plot(X, model, mesh_limits=mesh_limits, ax=ax, resolution=100)
-
-    iris = load_iris()
-    X, y = iris.data, iris.target
-    model = LogisticRegression().fit(X, y)
-    ax = plt.subplot2grid([2, 2], (1, 0))
-    mesh_limits = (np.min(X[:, 0]) - .5, np.max(X[:, 0]) + .5, np.min(X[:, 1]) - .5, np.max(X[:, 1]) + .5)
-    nd_boundary_plot(X, model, mesh_limits=mesh_limits, ax=ax, resolution=100)
-
-    X, y = make_circles()
-    model = BaggingClassifier().fit(X, y)
-    ax = plt.subplot2grid([2, 2], (0, 1))
-    mesh_limits = (np.min(X[:, 0]) - .5, np.max(X[:, 0]) + .5, np.min(X[:, 1]) - .5, np.max(X[:, 1]) + .5)
-    nd_boundary_plot(X, model, mesh_limits=mesh_limits, ax=ax, resolution=100)
+    nd_boundary_plot(X, model.predict(X), model, ax=ax, resolution=100)
 
     X, y = make_circles()
     model = KNeighborsClassifier().fit(X, y)
     ax = plt.subplot2grid([2, 2], (1, 1))
     mesh_limits = (np.min(X[:, 0]) - .5, np.max(X[:, 0]) + .5, np.min(X[:, 1]) - .5, np.max(X[:, 1]) + .5)
-    nd_boundary_plot(X, model, mesh_limits=mesh_limits, ax=ax, resolution=100)
+    nd_boundary_plot(X, model.predict(X), model, ax=ax, resolution=100)
 
     plt.tight_layout()
     plt.show()
